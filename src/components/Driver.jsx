4de5d6a275ab5994b8fb9824abfd88d5b98a23ba
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-08-05 19:24:14 -0500

    [BUILD]

--- src/components/Driver.jsx
+++ src/components/Driver.jsx
@@ -7,12 +7,12 @@ export default function Example() {
 		<>
 			<div className="mt-20 flex flex-wrap items-center justify-center gap-12">
 				{data.map((item) => (
-					<div className={`border-r-2 border-t-2 rounded-xl rounded-ss-none rounded-ee-none pr-4 pt-4 hover:border-${item.color}`}>
+					<div className={`border-r-2 border-t-2 rounded-xl rounded-ss-none rounded-ee-none pr-4 pt-4 hover:border-gray-400`}>
 
-					<section className="flex h-[20.4rem] w-[26rem] flex-col mt-20 relative bottom-20">
+					<section className="flex h-[20.4rem] hover:h-[21.5rem] duration-150 w-[26rem] flex-col mt-20 relative bottom-20 hover:bottom-[3.9rem]">
 						<div className="flex justify-between">
 							<div className='flex'>
-								<div className={`bg-${item.color} h-9 w-1`}></div>
+								<div className={`bg-white h-9 w-1`}></div>
 								<div className="relative bottom-1 mb-6 ml-3 h-9">
 									<p>{item.first_name}</p>
 									<p className="font-black">{item.last_name}</p>
@@ -24,7 +24,7 @@ export default function Example() {
 						<div>
 							<p>{item.team_name}</p>
 							<div className='flex items-end'>
-								<img className='h-20 relative right-12 top-2' src={item.driver_number_img} alt={item.driver_number} />
+								<img className='h-20 relative top-2' src={item.driver_number_img} alt={item.driver_number} />
 								<img className='h-80 relative right-7' src={item.headshot_url} alt={item.full_name} />
 							</div>
 						</div>
