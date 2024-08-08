ae1c345ef9053b6a5e2204af67c686dcb8b348c7
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-08-08 15:50:09 -0500

    [FIX] Formate code

--- src/components/Driver.jsx
+++ src/components/Driver.jsx
@@ -5,29 +5,37 @@ import data from './data.json';
 export default function Example() {
 	return (
 		<>
-			<div className="mt-20 flex flex-wrap items-center justify-center gap-8 sm:gap-4 sm:gap-x-12 gap-x-11 mx-1">
+			<div className="mx-1 mt-20 flex flex-wrap items-center justify-center gap-8 gap-x-11 sm:gap-4 sm:gap-x-12">
 				{data.map((item) => (
 					<div>
-					<section className="flex h-[26.41rem] hover:h-[27.5rem] duration-150 w-[26rem] flex-col mt-8 relative bottom-20 hover:bottom-[3.9rem] hover:mt-[-1.1rem]  hover:pt-[2rem] border-r-2 border-t-2 rounded-xl rounded-ss-none rounded-ee-none pr-4 pt-4 hover:border-gray-400">
-						<div className="flex justify-between">
-							<div className='flex'>
-								<div className={`bg-white h-9 w-1`}></div>
-								<div className="relative bottom-1 mb-6 ml-3 h-9">
-									<p>{item.first_name}</p>
-									<p className="font-black">{item.last_name}</p>
+						<section className="relative bottom-20 mt-8 flex h-[26.41rem] w-[26rem] flex-col rounded-xl rounded-ee-none rounded-ss-none border-r-2 border-t-2 pr-4 pt-4 duration-150 hover:bottom-[3.9rem] hover:mt-[-1.1rem] hover:h-[27.5rem] hover:border-gray-400 hover:pt-[2rem]">
+							<div className="flex justify-between">
+								<div className="flex">
+									<div className={`h-9 w-1 bg-white`}></div>
+									<div className="relative bottom-1 mb-6 ml-3 h-9">
+										<p>{item.first_name}</p>
+										<p className="font-black">{item.last_name}</p>
+									</div>
+								</div>
+								<img className="h-9" src={item.country} alt={item.nameC} />
+							</div>
+							<hr className="w-auto text-white" />
+							<div>
+								<p>{item.team_name}</p>
+								<div className="flex items-end">
+									<img
+										className="relative right-2 top-2 h-20"
+										src={item.driver_number_img}
+										alt={item.driver_number}
+									/>
+									<img
+										className="relative right-10 h-80"
+										src={item.headshot_url}
+										alt={item.full_name}
+									/>
 								</div>
 							</div>
-							<img className="h-9" src={item.country} alt={item.nameC} />
-						</div>
-						<hr className="w-auto text-white" />
-						<div>
-							<p>{item.team_name}</p>
-							<div className='flex items-end'>
-								<img className='h-20 relative top-2 right-2' src={item.driver_number_img} alt={item.driver_number} />
-								<img className='h-80 relative right-10' src={item.headshot_url} alt={item.full_name} />
-							</div>
-						</div>
-					</section>
+						</section>
 					</div>
 				))}
 			</div>
