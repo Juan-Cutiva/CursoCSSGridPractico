2cb47758eba77a501fe70fc9bc6544c6033be601
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-08-08 18:14:19 -0500

    [EXPERIMENT]

--- src/components/Driver.jsx
+++ src/components/Driver.jsx
@@ -3,22 +3,31 @@ import { Fragment, useState } from 'react';
 import data from './data.json';
 
 export default function Example() {
-	const prueba = data.map((item)=> item.color)
-	const [color] = useState("blue");
-	const test = "bg-blue-500";
-	const[color2] = useState("green");
-	const test2 = "hover:border-green-400";
-	const prueba2 = document.getElementById("llave")
-	console.log(prueba2)
+	const [color] = useState('blue');
+	const test = 'bg-blue-500';
+	const [color2] = useState('green');
+	const test2 = 'hover:border-green-400';
 
+	// if ( 0 === key){
+	// 	const [color] = useState("blue");
+	// 	const test = "bg-blue-500";
+	// 	const[color2] = useState("green");
+	// 	const test2 = "hover:border-green-400";
+	// }else if( 1 === key ){
+	// 	const [color] = useState("red");
+	// 	const test = "bg-red-500";
+	// 	const[color2] = useState("red");
+	// 	const test2 = "hover:border-red-400";
+	// }
 
 	return (
 		<>
 			<div className="mx-1 mt-20 flex flex-wrap items-center justify-center gap-8 gap-x-11 sm:gap-4 sm:gap-x-12">
-				
 				{data.map((item) => (
-					<div id='llave' key={item.key}>
-						<section className={`relative bottom-20 mt-8 flex h-[26.41rem] w-[26rem] flex-col rounded-xl rounded-ee-none rounded-ss-none border-r-2 border-t-2 pr-4 pt-4 duration-150 hover:bottom-[3.9rem] hover:mt-[-1.1rem] hover:h-[27.5rem] hover:border-${color2}-400 hover:pt-[2rem]`}>
+					<div id="llave" key={item.key}>
+						<section
+							className={`relative bottom-20 mt-8 flex h-[26.41rem] w-[26rem] flex-col rounded-xl rounded-ee-none rounded-ss-none border-r-2 border-t-2 pr-4 pt-4 duration-150 hover:bottom-[3.9rem] hover:mt-[-1.1rem] hover:h-[27.5rem] hover:border-${color2}-400 hover:pt-[2rem]`}
+						>
 							<div className="flex justify-between">
 								<div className="flex">
 									<div className={`h-9 w-1 bg-${color}-500`}></div>
