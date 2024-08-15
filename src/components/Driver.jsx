fa852785060314319aed4f4d044ef04a04234322
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-08-14 19:55:43 -0500

    "Updated settings.json with new driver names and refactored Driver.jsx component with improved formatting and syntax."

--- src/components/Driver.jsx
+++ src/components/Driver.jsx
@@ -3,52 +3,103 @@ import { Fragment } from 'react';
 import data from './data.json';
 
 export default function Example() {
-
 	return (
 		<>
 			<div className="mx-1 mt-20 flex flex-wrap items-center justify-center gap-8 gap-x-11 sm:gap-4 sm:gap-x-12">
-				{data.map(item =>{
+				{data.map((item) => {
+					const classNameBG =
+						item.team_name === 'Red Bull Racing'
+							? 'bg-[#3671c6]'
+							: '' || item.team_name === 'McLaren'
+								? 'bg-[#ff8000]'
+								: '' || item.team_name === 'Ferrari'
+									? 'bg-[#e80020]'
+									: '' || item.team_name === 'Mercedes'
+										? 'bg-[#27f4d2]'
+										: '' || item.team_name === 'Aston Martin'
+											? 'bg-[#229971]'
+											: '' || item.team_name === 'Haas'
+												? 'bg-[#b6babd]'
+												: '' || item.team_name === 'RB'
+													? 'bg-[#6692ff]'
+													: '' || item.team_name === 'Alpine'
+														? 'bg-[#0093cc]'
+														: '' || item.team_name === 'Williams'
+															? 'bg-[#64c4ff]'
+															: '' || item.team_name === 'Kick Sauber'
+																? 'bg-[#87d68a]'
+																: '';
 
-					const classNameBG = item.team_name === 'Red Bull Racing' ? "bg-[#3671c6]" : "" || item.team_name === 'McLaren' ? "bg-[#ff8000]" : "" || item.team_name === 'Ferrari' ? "bg-[#e80020]" : "" || item.team_name === 'Mercedes' ? "bg-[#27f4d2]" : "" || item.team_name === 'Aston Martin' ? "bg-[#229971]" : "" || item.team_name === 'Haas' ? "bg-[#b6babd]" : "" || item.team_name === 'RB' ? "bg-[#6692ff]" : "" || item.team_name === 'Alpine' ? "bg-[#0093cc]" : "" || item.team_name === 'Williams' ? "bg-[#64c4ff]" : "" || item.team_name === 'Kick Sauber' ? "bg-[#87d68a]" : ""
+					const classNameB =
+						item.team_name === 'Red Bull Racing'
+							? 'hover:border-[#3671c6]'
+							: '' || item.team_name === 'McLaren'
+								? 'hover:border-[#ff8000]'
+								: '' || item.team_name === 'Ferrari'
+									? 'hover:border-[#e80020]'
+									: '' || item.team_name === 'Mercedes'
+										? 'hover:border-[#27f4d2]'
+										: '' || item.team_name === 'Aston Martin'
+											? 'hover:border-[#229971]'
+											: '' || item.team_name === 'Haas'
+												? 'hover:border-[#b6babd]'
+												: '' || item.team_name === 'RB'
+													? 'hover:border-[#6692ff]'
+													: '' || item.team_name === 'Alpine'
+														? 'hover:border-[#0093cc]'
+														: '' || item.team_name === 'Williams'
+															? 'hover:border-[#64c4ff]'
+															: '' || item.team_name === 'Kick Sauber'
+																? 'hover:border-[#87d68a]'
+																: '';
 
-					const classNameB = item.team_name === 'Red Bull Racing' ? "hover:border-[#3671c6]" : "" || item.team_name === 'McLaren' ? "hover:border-[#ff8000]" : "" || item.team_name === 'Ferrari' ? "hover:border-[#e80020]" : "" || item.team_name === 'Mercedes' ? "hover:border-[#27f4d2]" : "" || item.team_name === 'Aston Martin' ? "hover:border-[#229971]" : "" || item.team_name === 'Haas' ? "hover:border-[#b6babd]" : "" || item.team_name === 'RB' ? "hover:border-[#6692ff]" : "" || item.team_name === 'Alpine' ? "hover:border-[#0093cc]" : "" || item.team_name === 'Williams' ? "hover:border-[#64c4ff]" : "" || item.team_name === 'Kick Sauber' ? "hover:border-[#87d68a]" : ""
-
-					const location = item.location === '0' ? "right-10" : "" || item.location === '1' ? "right-12" : "" || item.location === '2' ? "right-6" : "" || item.location === '3' ? "right-14" : "" || item.location === '4' ? "right-16" : ""
+					const location =
+						item.location === '0'
+							? 'right-10'
+							: '' || item.location === '1'
+								? 'right-12'
+								: '' || item.location === '2'
+									? 'right-6'
+									: '' || item.location === '3'
+										? 'right-14'
+										: '' || item.location === '4'
+											? 'right-16'
+											: '';
 
 					return (
 						<div id="llave" key={item.key}>
-						<section
-							className={`relative bottom-20 mt-8 flex h-[24.4rem] sm:h-[26.41rem] w-[22rem] sm:w-[26rem] flex-col rounded-xl rounded-ee-none rounded-ss-none border-r-2 border-t-2 pr-4 pt-4 duration-150 sm:hover:bottom-[3.9rem] sm:hover:mt-[-1.1rem] sm:hover:h-[27.5rem] hover:h-[25.5rem] hover:pt-[2rem] ${classNameB}`}
-						>
-							<div className="flex justify-between">
-								<div className="flex">
-									<div className={` h-9 w-1 ${classNameBG}`}></div>
-									<div className="relative bottom-1 mb-6 ml-3 h-9">
-										<p className='text-gray-100'>{item.first_name}</p>
-										<p className="font-black text-gray-100">{item.last_name}</p>
+							<section
+								className={`relative bottom-20 mt-8 flex h-[24.4rem] w-[22rem] flex-col rounded-xl rounded-ee-none rounded-ss-none border-r-2 border-t-2 pr-4 pt-4 duration-150 hover:h-[25.5rem] hover:pt-[2rem] sm:h-[26.41rem] sm:w-[26rem] sm:hover:bottom-[3.9rem] sm:hover:mt-[-1.1rem] sm:hover:h-[27.5rem] ${classNameB}`}
+							>
+								<div className="flex justify-between">
+									<div className="flex">
+										<div className={`h-9 w-1 ${classNameBG}`}></div>
+										<div className="relative bottom-1 mb-6 ml-3 h-9">
+											<p className="text-gray-100">{item.first_name}</p>
+											<p className="font-black text-gray-100">{item.last_name}</p>
+										</div>
+									</div>
+									<img className="h-9 rounded border-[1px]" src={item.country} alt={item.nameC} />
+								</div>
+								<hr className="w-auto text-white" />
+								<div>
+									<p className="text-gray-100">{item.team_name}</p>
+									<div className="flex items-end">
+										<img
+											className="relative right-2 top-1 h-14 sm:h-20"
+											src={item.driver_number_img}
+											alt={item.driver_number}
+										/>
+										<img
+											className={`relative ${location} h-72 sm:h-80`}
+											src={item.headshot_url}
+											alt={item.full_name}
+										/>
 									</div>
 								</div>
-								<img className="h-9 rounded border-[1px]" src={item.country} alt={item.nameC} />
-							</div>
-							<hr className="w-auto text-white" />
-							<div>
-								<p className='text-gray-100'>{item.team_name}</p>
-								<div className="flex items-end">
-									<img
-										className="relative right-2 top-1 h-14 sm:h-20 "
-										src={item.driver_number_img}
-										alt={item.driver_number}
-									/>
-									<img
-										className={`relative ${location} h-72 sm:h-80`}
-										src={item.headshot_url}
-										alt={item.full_name}
-									/>
-								</div>
-							</div>
-						</section>
-					</div>
-					)
+							</section>
+						</div>
+					);
 				})}
 			</div>
 		</>
---
+++ src/components/Escuderia.jsx
@@ -0,0 +1,106 @@
+import { Fragment } from 'react';
+
+import data from './escuderias.json';
+
+export default function Example() {
+	return (
+		<>
+			<div className="mx-1 mt-20 flex flex-wrap items-center justify-center gap-8 gap-x-11 sm:gap-4 sm:gap-x-12">
+				{data.map((item) => {
+					const classNameBG =
+						item.name === 'Red Bull Racing'
+							? 'bg-[#3671c6]'
+							: '' || item.name === 'McLaren'
+								? 'bg-[#ff8000]'
+								: '' || item.name === 'Ferrari'
+									? 'bg-[#e80020]'
+									: '' || item.name === 'Mercedes'
+										? 'bg-[#27f4d2]'
+										: '' || item.name === 'Aston Martin'
+											? 'bg-[#229971]'
+											: '' || item.name === 'Haas'
+												? 'bg-[#b6babd]'
+												: '' || item.name === 'RB'
+													? 'bg-[#6692ff]'
+													: '' || item.name === 'Alpine'
+														? 'bg-[#0093cc]'
+														: '' || item.name === 'Williams'
+															? 'bg-[#64c4ff]'
+															: '' || item.name === 'Kick Sauber'
+																? 'bg-[#87d68a]'
+																: '';
+
+					const classNameB =
+						item.name === 'Red Bull Racing'
+							? 'hover:border-[#3671c6]'
+							: '' || item.name === 'McLaren'
+								? 'hover:border-[#ff8000]'
+								: '' || item.name === 'Ferrari'
+									? 'hover:border-[#e80020]'
+									: '' || item.name === 'Mercedes'
+										? 'hover:border-[#27f4d2]'
+										: '' || item.name === 'Aston Martin'
+											? 'hover:border-[#229971]'
+											: '' || item.name === 'Haas'
+												? 'hover:border-[#b6babd]'
+												: '' || item.name === 'RB'
+													? 'hover:border-[#6692ff]'
+													: '' || item.name === 'Alpine'
+														? 'hover:border-[#0093cc]'
+														: '' || item.name === 'Williams'
+															? 'hover:border-[#64c4ff]'
+															: '' || item.name === 'Kick Sauber'
+																? 'hover:border-[#87d68a]'
+																: '';
+
+					const location =
+						item.location === '0'
+							? 'right-10'
+							: '' || item.location === '1'
+								? 'right-12'
+								: '' || item.location === '2'
+									? 'right-6'
+									: '' || item.location === '3'
+										? 'right-14'
+										: '' || item.location === '4'
+											? 'right-16'
+											: '';
+
+					return (
+						<div id="llave" key={item.key}>
+							<section
+								className={`relative bottom-20 mt-8 flex h-[23.4rem] w-[36rem] flex-col rounded-xl rounded-ee-none rounded-ss-none border-r-2 border-t-2 pr-4 pt-3 duration-150 hover:h-[24.5rem] hover:pt-[1rem] sm:h-[25.41rem] sm:w-[40rem] sm:hover:bottom-[4.9rem] sm:hover:mt-[0.782rem] sm:hover:h-[26.5rem] ${classNameB}`}
+							>
+								<div className="flex items-center">
+									<div className={`mr-3 h-9 w-1 ${classNameBG}`}></div>
+									<p className="w-[90%]">{item.name}</p>
+									<img src={item.logo} alt={item.name} className="w-[10%]" />
+								</div>
+
+								<div className="flex gap-[10%]">
+									<div className="flex w-[45%] items-center justify-between">
+										<div className="flex">
+											<p>{item.driver1}</p>
+											<p className="pl-2">{item.driver1A}</p>
+										</div>
+										<img src={item.imgD1} alt={item.driver1} className="h-12" />
+									</div>
+									<div className="flex w-[45%] items-center justify-between">
+										<div className="flex">
+											<p>{item.driver2}</p>
+											<p className="pl-2">{item.driver2A}</p>
+										</div>
+										<img src={item.imgD2} alt={item.driver2} className="h-12" />
+									</div>
+								</div>
+								<div className='flex justify-center'>
+									<img src={item.Car} alt={item.name} className="w-[30rem]" />
+								</div>
+							</section>
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
+}
