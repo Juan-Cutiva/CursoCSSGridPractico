50b42e624b20a3920cebc6fe57492d1e0a6a5bbb
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-08-16 12:37:33 -0500

    [BUILD]

--- src/components/Escuderia.jsx
+++ src/components/Escuderia.jsx
@@ -70,7 +70,7 @@ export default function Example() {
 					return (
 						<div id="llave" key={item.key}>
 							<section
-								className={`text-gray-100 relative bottom-20 mt-8 flex h-[18.5rem] w-[36rem] flex-col rounded-xl rounded-ee-none rounded-ss-none border-r-2 border-t-2 pr-4 pt-3 duration-150 hover:h-[19.3rem] hover:pt-[2rem] sm:h-[20rem] sm:w-[40rem] sm:hover:bottom-[3rem] sm:hover:mt-[-3.2rem] sm:hover:h-[21.3rem] ${classNameB}`}
+								className={`text-gray-100 relative bottom-20 mt-8 flex h-[18.5rem] w-[22rem] flex-col rounded-xl rounded-ee-none rounded-ss-none border-r-2 border-t-2 pr-4 pt-3 duration-150 hover:h-[19.3rem] hover:pt-[2rem] sm:h-[20rem] sm:w-[40rem] sm:hover:bottom-[3rem] sm:hover:mt-[-3.2rem] sm:hover:h-[21.3rem] ${classNameB}`}
 							>
 								<div className="flex items-center">
 									<div className={`mr-3 h-9 w-1 ${classNameBG}`}></div>
@@ -78,26 +78,26 @@ export default function Example() {
 									<img src={item.logo} alt={item.name} className="w-[10%]" />
 								</div>
 								
-								<hr className="w-auto text-gray-100 mb-2"/>
+								<hr className="w-auto text-gray-100 mb-4 sm:mb-2 mt-3 sm:mt-0"/>
 
 								<div className="flex gap-[10%]">
 									<div className="flex w-[45%] items-center justify-between border-b-[1px] border-r-[1px] rounded-br-xl">
-										<div className="flex">
+										<div className="flex flex-col sm:flex-row">
 											<p className="tracking-[0.0125rem]">{item.driver1}</p>
-											<p className="pl-2 tracking-[0.0125rem] font-bold">{item.driver1A}</p>
+											<p className="sm:pl-2 tracking-[0.0125rem] font-bold">{item.driver1A}</p>
 										</div>
-										<img src={item.imgD1} alt={item.driver1} className="mr-[0.4rem] h-12" />
+										<img src={item.imgD1} alt={item.driver1} className="mr-[0.4rem] h-12 hidden sm:block" />
 									</div>
 									<div className="flex w-[45%] items-center justify-between border-b-[1px] border-r-[1px] rounded-br-xl">
-										<div className="flex">
+										<div className="flex flex-col sm:flex-row">
 											<p className="tracking-[0.0125rem]">{item.driver2}</p>
-											<p className="pl-2 tracking-[0.0125rem] font-bold">{item.driver2A}</p>
+											<p className="sm:pl-2 tracking-[0.0125rem] font-bold">{item.driver2A}</p>
 										</div>
-										<img src={item.imgD2} alt={item.driver2} className="mr-[0.4rem] h-12" />
+										<img src={item.imgD2} alt={item.driver2} className="mr-[0.4rem] h-12 hidden sm:block" />
 									</div>
 								</div>
 								<div id="img" className="mt-4 flex justify-center rounded-xl pt-8">
-									<img src={item.Car} alt={item.name} className="max-w-[30rem] ml-5" />
+									<img src={item.Car} alt={item.name} className="max-w-[30rem] min-w-[21rem] ml-2 sm:ml-5" />
 								</div>
 							</section>
 						</div>
