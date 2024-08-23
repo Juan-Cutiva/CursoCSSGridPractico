264be85f2378d10879416b611be60c5b00fdf0aa
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-08-23 15:00:04 -0500

    [FIX] Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,9 +36,9 @@ export default function Example() {
 		<Disclosure as="nav" className="bg-red-500">
 			{({ open }) => (
 				<>
-					<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+					<div className="mx-auto max-w-7xl px-2 md:px-6 lg:px-8">
 						<div className="relative flex h-16 items-center justify-between">
-							<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+							<div className="absolute inset-y-0 left-0 flex items-center md:hidden">
 								{/* Mobile menu button*/}
 								<DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-900 hover:bg-red-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-500">
 									<span className="absolute -inset-0.5" />
@@ -50,7 +50,7 @@ export default function Example() {
 									)}
 								</DisclosureButton>
 							</div>
-							<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+							<div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
 								<a href="/" className="flex flex-shrink-0 items-center">
 									<img
 										className="h-8 w-full"
@@ -58,7 +58,7 @@ export default function Example() {
 										alt="F1 Logo"
 									/>
 								</a>
-								<div className="hidden sm:ml-6 sm:block">
+								<div className="hidden md:ml-6 md:block">
 									<div className="flex space-x-4">
 										{navigation.map((item) => (
 											<a
@@ -80,7 +80,7 @@ export default function Example() {
 									</div>
 								</div>
 							</div>
-							<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+							<div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
 								{/* Profile dropdown */}
 								<Menu as="div" className="relative ml-3">
 									<div>
@@ -181,7 +181,7 @@ export default function Example() {
 						</div>
 					</div>
 
-					<DisclosurePanel className="sm:hidden">
+					<DisclosurePanel className="md:hidden">
 						<div className="space-y-1 px-2 pb-3 pt-2">
 							{navigation.map((item) => (
 								<DisclosureButton
